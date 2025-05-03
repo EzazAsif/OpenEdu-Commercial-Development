5d499ae60c8d9565fef311b61979fcf2c5209e83
Ezaz Asif <asifrafid8399@gmail.com>
2025-05-04 01:51:22 +0600

    websocker connection established

--- gitlms/gitlms/asgi.py
+++ gitlms/gitlms/asgi.py
@@ -4,14 +4,17 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from notifications.routing import websocket_urlpatterns as notifications_urlpatterns  # Import WebSocket routing for notifications
 from commChat.routing import websocket_urlpatterns as commchat_urlpatterns  # Import WebSocket routing for commChat
-
+from commChat.consumers import *
+from django.urls import path
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gitlms.settings')
+
+
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),  # For HTTP requests
     "websocket": AuthMiddlewareStack(  # For WebSocket requests
         URLRouter(
-            notifications_urlpatterns + commchat_urlpatterns  # Combine both WebSocket URL patterns
+            commchat_urlpatterns + notifications_urlpatterns  # Combine both WebSocket URL patterns
         )
     ),
 })
