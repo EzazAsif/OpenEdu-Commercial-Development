be2cbba8149c094af0d02b040878451a2c37f4e3
Ezaz Asif <asifrafid8399@gmail.com>
2025-05-04 11:54:45 +0600

    websocket recieves messeges

--- gitlms/commChat/consumers.py
+++ gitlms/commChat/consumers.py
@@ -25,7 +25,7 @@ class CommChatConsumer(AsyncWebsocketConsumer):
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
-
+        print(message)
         # Send message to WebSocket
         await self.send(text_data=json.dumps({
             'message': message
