aa20c772c5e2f4537793802873f2451fc35f4ab0
Ezaz Asif <asifrafid8399@gmail.com>
2025-05-13 17:51:40 +0600

    fixed the signals issue

--- gitlms/accounts/signals.py
+++ gitlms/accounts/signals.py
@@ -1,61 +1,29 @@
-from social_django.signals import pre_social_login
+from django.contrib.auth.signals import user_logged_in
 from django.dispatch import receiver
-from django.contrib.auth import get_user_model
 from social_django.models import UserSocialAuth
-from django.contrib.auth.models import User
 
-@receiver(pre_social_login)
-def social_user_connected(sender, request, socialauth, **kwargs):
-    """Link social account to existing user or create a new user."""
-    UserModel = get_user_model()  # Get the custom user model
+@receiver(user_logged_in)
+def social_user_connected(sender, request, user, **kwargs):
+    """Link social account to existing user or create a new user in custom model."""
+    
+    # We only want to handle social login users
+    try:
+        # Retrieve the social authentication user associated with the logged-in user
+        social_user = UserSocialAuth.objects.get(user=user)
 
-    # If the user already exists, we associate the social account with them
-    if socialauth.is_existing:
-        return
+        # Only update if the username is not set (to avoid overwriting if already set)
+        if user.username != user.email:
+            # Update the username to be the user's email
+            user.username = user.email  # Set username to the user's email
+            user.first_name = social_user.extra_data.get('first_name', '')  # Get first name from social data
+            user.last_name = social_user.extra_data.get('family_name', '')  # Use family_name for last name
+            
+            # Optionally, store other fields like the profile picture URL
+            user.profile_picture = social_user.extra_data.get('picture', '')  # Save profile picture from Google data
 
-    # If the user doesn't exist, create a new user using the social auth data
-    if socialauth.provider == 'google':
-        email = socialauth.extra_data.get('email')
-        username = email.split('@')[0]  # Generate username from email
-        first_name = socialauth.extra_data.get('name')
-        # Create a new user and link it to the social account
-        user = UserModel.objects.create_user(
-            username=username,
-            email=email,
-            first_name=first_name,
-            password=None  # Don't set a password, as this is for social login
-        )
-        socialauth.user = user
-        socialauth.save()
+            # Save the updated user information
+            user.save()
 
-@receiver(pre_social_login)
-def social_user_connected(sender, request, socialauth, **kwargs):
-    """Link social account to existing user or create a new user."""
-    UserModel = get_user_model()  # Get the custom user model
-
-    # If the user already exists, we associate the social account with them
-    if socialauth.is_existing:
-        return
-
-    # If the user doesn't exist, create a new user using the social auth data
-    if socialauth.provider == 'google':
-        email = socialauth.extra_data.get('email')
-        username = email.split('@')[0]  # Generate username from email
-        first_name = socialauth.extra_data.get('name')
-        last_name = socialauth.extra_data.get('family_name', '')
-        picture_url = socialauth.extra_data.get('picture', '')  # Google profile picture
-
-        # Create a new user and link it to the social account
-        user = UserModel.objects.create_user(
-            username=username,
-            email=email,
-            first_name=first_name,
-            last_name=last_name,
-            password=None  # Don't set a password, as this is for social login
-        )
-        # Save the picture URL or other info to the user model
-        user.profile_picture = picture_url
-        user.save()
-
-        socialauth.user = user
-        socialauth.save()
+    except UserSocialAuth.DoesNotExist:
+        # User did not log in through social auth, do nothing
+        pass
