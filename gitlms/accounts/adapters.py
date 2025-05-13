4cf2926d1ced063ab4439022c3e321282d483c65
Ezaz Asif <asifrafid8399@gmail.com>
2025-05-14 02:36:06 +0600

    hoping to work

--- gitlms/accounts/adapters.py
+++ gitlms/accounts/adapters.py
@@ -1,17 +1,37 @@
-from allauth.account.adapter import DefaultAccountAdapter
-from .models import User
+from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
+from django.contrib.auth import get_user_model
+from allauth.socialaccount.models import SocialAccount
+from django.http import Http404
 
-class CustomAccountAdapter(DefaultAccountAdapter):
-    def is_open_for_signup( request,sociallogin):
-        print(request,sociallogin)
-        """
-        By overriding this method, you can skip the sign-up page
-        and directly log in the user if the email already exists.
-        """
-        email = sociallogin.user.email
-        try:
-            user = User.objects.get(email=email)
-            # If the user already exists, return False to skip signup and log them in
-            return False
-        except User.DoesNotExist:
-            return True
+User = get_user_model()
+
+class MySocialAccountAdapter(DefaultSocialAccountAdapter):
+    def pre_social_login(self, request, sociallogin=None):
+        if sociallogin is None:
+            # Manually get sociallogin from request (this is more of a fallback for your case)
+            sociallogin = request.socialaccount
+        if not sociallogin:
+            raise Http404("No social login found")
+
+        # If the user is already existing, do nothing
+        if sociallogin.is_existing:
+            return
+
+        email = sociallogin.account.extra_data.get('email')
+        if email:
+            try:
+                # Attempt to find the existing user by email
+                user = User.objects.get(email=email)
+                sociallogin.user = user  # Link this social login to the user
+
+                # Save the social account so the link persists in future logins
+                sociallogin.save()
+
+                # Optionally, you can create a SocialAccount if it doesn't exist
+                social_account, created = SocialAccount.objects.get_or_create(user=user, provider=sociallogin.account.provider)
+                if created:
+                    social_account.save()
+
+            except User.DoesNotExist:
+                pass  # Do nothing if user doesn't exist
+
