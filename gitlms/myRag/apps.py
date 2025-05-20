d9822f136dac2c50e8d3e4861546d03e424d36db
Ezaz Asif <asifrafid8399@gmail.com>
2025-05-20 16:28:42 +0600

    implementing rag

---
+++ gitlms/myRag/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MyragConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'myRag'
---
+++ gitlms/myRag/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from .qdrant_helper import search_qdrant
+
+def semantic_search(request):
+    query = request.GET.get("q", "")
+    if not query:
+        return JsonResponse({"error": "Query not provided"}, status=400)
+    results = search_qdrant("lecture_materials", query)
+    formatted = [
+        {
+            "title": r.payload.get("title"),
+            "faculty": r.payload.get("faculty"),
+            "uploaded_by": r.payload.get("uploaded_by"),
+            "score": r.score
+        }
+        for r in results
+    ]
+    return JsonResponse({"results": formatted})
